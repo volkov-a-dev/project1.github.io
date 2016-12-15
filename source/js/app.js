60e19d8ed57390ff07ce784e521adc21ade74afc
a.volkov.linux <alex@a.volko.t>
2016-12-15 19:20:41 +0300

    Add flip + style welcome

--- source/js/app.js
+++ source/js/app.js
@@ -4,4 +4,22 @@
   setTimeout(function() {
     document.querySelector('.greating_picture').classList.add('m--show');
   }, 1000);
+
+  console.log('test');
+
+  $('.btn-link').click(
+      function () {
+          console.log('ok click !!');
+          $('.flip-container__flipper').addClass('flip');
+          $(this).hide();
+      }
+  );
+
+  $('.btn-return').click(
+    function () {
+      console.log(' delete !!');
+        $('.flip-container__flipper').removeClass('flip');
+        $('.btn-link').show();
+        }
+    )
 })();
